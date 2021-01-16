b468ef2464a5884f3816b8379421777d038fceed
beaujeffrey03 <75648641+beaujeffrey03@users.noreply.github.com>
2021-01-15 23:45:11 -0600

    Stuck finding values of first and last row.

---
+++ PyBank/main.py
@@ -0,0 +1,42 @@
+import os
+
+import csv
+
+csvpath = os.path.join('Resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    total_months = 0
+    total = 0
+    average_change = 0
+    month_increase = 0
+    greatest_increase = 0
+    month_decrease = 0
+    greatest_decrease = 0
+
+    for row in csvreader:
+
+        month_col = str(row[0])
+        profit_loss_col = int(row[1])
+
+        total_months += 1
+
+        total += profit_loss_col
+
+        # average_change = (first_row - last_row) / (total_months - 1)
+
+    print('')
+    print('Financial Analysis')
+    print('--------------------------------')
+    print(f'Total Months: {total_months}')
+    print(f'Total: ${total}')
+    print(f'Average Change: {average_change}')
+    print(f'Greatest Increase in Profits: {month_increase} ({greatest_increase})')
+    print(f'Greatest Decrease in Profits: {month_decrease} ({greatest_decrease})')
---
+++ PyPoll/main.py
@@ -0,0 +1,35 @@
+import os
+
+import csv
+
+csvpath = os.path.join('Resources', '02-Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    total_votes = 0
+    candidate_name = 0
+    percent_votes = 0
+    count_votes = 0
+    winner = 0
+
+    for row in csvreader:
+
+        total_votes += 1
+
+
+    print('')
+    print('Election Results')
+    print('--------------------------------')
+    print(f'Total Votes: {total_votes}')
+    print('--------------------------------')
+    print(f'{candidate_name}: {percent_votes} ({count_votes})')
+    print('--------------------------------')
+    print(f'Winner: {winner}')
+    print('--------------------------------')
